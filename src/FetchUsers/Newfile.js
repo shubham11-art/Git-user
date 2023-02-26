4fd3f57ae12584950c2cd6f33c18a1d314ba964e
Shubham <shubhamgunjal88@gmail.com>
2023-02-26 16:14:03 +0530

    again new file pushed

---
+++ src/FetchUsers/Newfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Newfile = () => {
+  return (
+    <div>
+      <h2>Pushing to repository</h2>
+      <h1>Again new push</h1>
+    </div>
+  );
+};
+
+export default Newfile;
