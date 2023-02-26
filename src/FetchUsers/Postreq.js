c2208d65947c7869b1ad01131aadf624b8e8678a
Shubham <shubhamgunjal88@gmail.com>
2023-02-26 11:01:53 +0530

    added table to bugfixing

--- src/FetchUsers/Postreq.js
+++ src/FetchUsers/Postreq.js
@@ -4,8 +4,14 @@ const Postreq = () => {
   return (
     <div>
       <h1> I am a post request</h1>
+     <table><tr></tr>
+	  </table>
+
+	  <h2>Hello World</h2>
     </div>
   );
 };
 
 export default Postreq;
+
+
